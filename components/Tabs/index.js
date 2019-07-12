0981f1f1b3dcd13b85014bd25dbde537129cc3ae
Chike Chris <cchike2017@gmail.com>
2019-07-12 14:21:25 -0400

    worked on the tab index.js file

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,33 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+const topics = document.querySelector(".topics");
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(res => {
+    console.log("data: ", res);
+    const topicObj = res.data.topics;
+    console.log(topicObj);
+    topicObj.forEach(topic => {
+      const tab = createTab(topic);
+      topics.appendChild(tab);
+    });
+  })
+  .catch(error => {
+    console.log("Error", error);
+  });
+
+function createTab(topicTab) {
+  // create elements
+  const tab = document.createElement("div");
+
+  // set classes
+  tab.classList.add("tab");
+
+  // set text content
+  tab.textContent = topicTab;
+
+  return tab;
+}
